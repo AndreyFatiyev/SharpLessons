788c0ab7d286bb10dfd1a76fee04c220cb9aed2e
Andrey Fatiyev <andreyfatiyev@gmail.com>
2022-09-04 17:39:09 +0400

    MethodLessons

---
+++ Методы/Метод/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace MyApp
+{
+    class Program
+    {
+        static string GetGreetingMessage(string name, double salary)
+        {
+            int salaryCeiling = (int)Math.Ceiling(salary);
+            string c = Convert.ToString(salaryCeiling);
+            return ("Hello,"+name+" your salary is "+c);
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(GetGreetingMessage("Student", 10.01));
+            Console.WriteLine(GetGreetingMessage("Bill Gates", 10000000.5));
+            Console.WriteLine(GetGreetingMessage("Steve Jobs", 1));
+
+
+
+        }
+    }
+}
