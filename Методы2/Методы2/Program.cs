ad3b09ec1d34ac9d9d6389f77fc7f8c29b154a82
Andrey Fatiyev <andreyfatiyev@gmail.com>
2022-09-04 23:28:25 +0400

    lesson2 added

---
+++ Методы2/Методы2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MyApp;
+using System;
+class Program
+{
+    static string GetLastHalf(string a)
+    {
+       int b = a.Length / 2;
+        a = a.Substring(b);
+        a = a.Replace("", " ");   
+        return a;
+    }
+    static void Main(string[] arghs)
+    {
+
+
+
+
+        Console.WriteLine(GetLastHalf("I love CSharp!"));
+        Console.WriteLine(GetLastHalf("1234567890"));
+        Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+
+
+
+
+
+
+    }
+}
