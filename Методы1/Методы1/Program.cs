d0c54dcd0f0666ffdc2aeac3d406d645e614286b
Andrey Fatiyev <andreyfatiyev@gmail.com>
2022-09-04 18:38:34 +0400

    lesson1 added

---
+++ Методы1/Методы1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MyApp
+{
+    class Program
+    {
+       
+        static int GetSquare(int a)
+        {
+            int b = a * a;
+            return b;
+
+        }
+        static string Print(int b)
+        {
+            string c = Convert.ToString(b);
+            string d = ("Squade of the number  = 42 is  ");
+            return (d+b );
+
+        }
+
+        static void Main(string[] arghs)
+        {            
+         
+           string SquadOfTheNum = Print(GetSquare(42));
+            Console.WriteLine(SquadOfTheNum);
+
+
+        }
+    }
+}
